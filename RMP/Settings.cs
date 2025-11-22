42f6fcd289c2a0855153d70cc9b38a5bfad37253
Jacob Stenborg McDermott <bymjodfgad@gmail.com>
2025-11-22 12:38:07 -0800

    First setting variables and folder paths for logs and music folder.

---
+++ RMP/Settings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMP
+{
+    public class Settings
+    {
+        public int Volume { get; set; } = 50;
+        public string MusicFolder { get; set; } =
+            Path.Combine(Environment.CurrentDirectory, "Music");
+
+        public string LogFolder { get; set; } =
+            Path.Combine(Environment.CurrentDirectory, "logs");
+
+        public string LastPlayedSong { get; set; } = "";
+        public double LastPosition { get; set; } = 0;
+    }
+}
